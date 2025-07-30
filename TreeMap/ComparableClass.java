504b1067fcaf7062e46cb00ab2a5f572cd156d05
hrutwik2026 <kalehrutwik@gmail.com>
2025-07-30 20:12:38 +0530

    TreeMap Examples using Comarable and Compartor

---
+++ TreeMap/ComparableClass.java
@@ -0,0 +1,36 @@
+package Collection.TreeMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ComparableClass {
+    public static void main(String[] args) {
+        TreeMap<Person,Integer> map=new TreeMap<>();
+        map.put(new Person("John", 22), 456);
+        map.put(new Person("Alice", 45), 88);
+        map.put(new Person("Kane", 62), 63);
+
+        for (Map.Entry<Person, Integer> p:map.entrySet()){
+            System.out.println("name :"+p.getKey().name+" Id:"+p.getKey().id+" marks:"+p.getValue());
+        }
+    }
+}
+class  Person implements Comparable<Person>{
+    String name;
+    int id;
+
+    Person(String name,int id){
+        this.id=id;
+        this.name=name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(this.id,o.id);
+    }
+
+    @Override
+    public String toString() {
+        return "name :"+name+" ID:"+id;
+    }
+}
